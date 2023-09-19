d135f3212f45049e27646a79fc6391d5ff06e2cb
Joey Rocha <jrochaperez123@gmail.com>
2023-09-19 10:44:22 -0700

    first commit

---
+++ assignment_1.1.js
@@ -0,0 +1,4 @@
+//This code was reference from https://github.com/su-w3-bootcamp/software-1.1-account-setup-and-git/blob/main/assignment.md
+function printMe() {
+    console.log("I am a reference example");
+}
